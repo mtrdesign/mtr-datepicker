6fcf1d5c0e263982e2c94ea31f3d0eac823698d4
Valentin Kirilov <val.kirilov@gmail.com>
2015-10-30 18:13:06 +0200

    Second travis test

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome'],
+
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
 
 
     // Continuous Integration mode
